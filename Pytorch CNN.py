c9e2b9c29580c467fbfd1d40d5e2ab16ea19fcde
Johan.Coetzee <jcoetzee609@gmail.com>
2019-06-10 16:44:25 +0200

    Update Pytorch CNN.py

--- Pytorch CNN.py
+++ Pytorch CNN.py
@@ -34,3 +34,88 @@ class NeuralNetwork(nn.Module):
         out = self.fc5(out)
 
         return out
+optimizer = optim.Adam(model.parameters(), lr=1e-6)
+criterion = nn.MSELoss()
+
+while iteration < model.number_of_iterations:
+    # get output from the neural network
+    output = model(state)[0]
+
+    # initialize action
+    action = torch.zeros([model.number_of_actions], dtype=torch.float32)
+    if torch.cuda.is_available():  # put on GPU if CUDA is available
+        action = action.cuda()
+
+    # epsilon greedy exploration
+    random_action = random.random() <= epsilon
+    if random_action:
+        print("Performed random action!")
+    action_index = [torch.randint(model.number_of_actions, torch.Size([]), dtype=torch.int)
+                    if random_action
+                    else torch.argmax(output)][0]
+
+    if torch.cuda.is_available():  # put on GPU if CUDA is available
+        action_index = action_index.cuda()
+
+    action[action_index] = 1
+
+    # get next state and reward
+    image_data_1, reward, terminal = game_state.frame_step(action)
+    image_data_1 = resize_and_bgr2gray(image_data_1)
+    image_data_1 = image_to_tensor(image_data_1)
+    state_1 = torch.cat((state.squeeze(0)[1:, :, :], image_data_1)).unsqueeze(0)
+
+    action = action.unsqueeze(0)
+    reward = torch.from_numpy(np.array([reward], dtype=np.float32)).unsqueeze(0)
+
+    # save transition to replay memory
+    replay_memory.append((state, action, reward, state_1, terminal))
+
+    # if replay memory is full, remove the oldest transition
+    if len(replay_memory) > model.replay_memory_size:
+        replay_memory.pop(0)
+
+    # epsilon annealing
+    epsilon = epsilon_decrements[iteration]
+
+    # sample random minibatch
+    minibatch = random.sample(replay_memory, min(len(replay_memory), model.minibatch_size))
+
+    # unpack minibatch
+    state_batch = torch.cat(tuple(d[0] for d in minibatch))
+    action_batch = torch.cat(tuple(d[1] for d in minibatch))
+    reward_batch = torch.cat(tuple(d[2] for d in minibatch))
+    state_1_batch = torch.cat(tuple(d[3] for d in minibatch))
+
+    if torch.cuda.is_available():  # put on GPU if CUDA is available
+        state_batch = state_batch.cuda()
+        action_batch = action_batch.cuda()
+        reward_batch = reward_batch.cuda()
+        state_1_batch = state_1_batch.cuda()
+
+    # get output for the next state
+    output_1_batch = model(state_1_batch)
+
+    # set y_j to r_j for terminal state, otherwise to r_j + gamma*max(Q)
+    y_batch = torch.cat(tuple(reward_batch[i] if minibatch[i][4]
+                              else reward_batch[i] + model.gamma * torch.max(output_1_batch[i])
+                              for i in range(len(minibatch))))
+
+    # extract Q-value
+    q_value = torch.sum(model(state_batch) * action_batch, dim=1)
+
+    # PyTorch accumulates gradients by default, so they need to be reset in each pass
+    optimizer.zero_grad()
+
+    # returns a new Tensor, detached from the current graph, the result will never require gradient
+    y_batch = y_batch.detach()
+
+    # calculate loss
+    loss = criterion(q_value, y_batch)
+
+    # do backward pass
+    loss.backward()
+    optimizer.step()
+
+    # set state to be state_1
+    state = state_1
